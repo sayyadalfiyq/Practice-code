0ce28867382e15351e1cdb01980316a646619a5f
alfiya sayyad <143630382+sayyadalfiyq@users.noreply.github.com>
2023-10-10 21:42:10 +0100

    fet added sample code

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const accountId = "1253"
+let accountEmail = "alfiya@gmail.com"
+var accountPassword = "1236"
+accountcity = "nagpur"
+
+// accountId = 4  double constant id not allowed it's a unique id. //
+
+console.log(accountId)
+/* prefer not u use var bcoz issues in function scope or block scope */
+
+ accountEmail = "alfi@gmailcom"
+ accountPassword = "9988"
+ accountcity = "bihar"
+
+ console.table([accountId, accountEmail, accountPassword , accountcity]);
---
+++ 02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat all code as a js newer version.//
+
+
+// alert (3 + 3)  we r using node js not browser //
+//alert(3 + 3) code  readable should be high // 
+
+console.log("alfiya");
+
+let name = "sayyad"
+let age = "23"
+let IsloggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => "" 
+// boolean  => true/false
+// null => standalone
+// undefined => undefined itself undefined data type ex. let state ""
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof "hitesh") // undefined data type //  
+console.log(typeof null) // object data type //
